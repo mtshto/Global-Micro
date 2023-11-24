a01738ae3994ea5ed81bac06f67121fd7cef4840
mtshto <mts.oliveira4320@gmail.com>
2023-11-23 21:43:28 -0300

    100% funcionando

--- src/util/db.js
+++ src/util/db.js
@@ -1,15 +1,17 @@
 // src/utils/db.js
 const { Sequelize } = require('sequelize');
 
+
+console.log('Host:', process.env.DB_HOST || 'oracle.fiap.com.br');
+console.log('Port:', process.env.DB_PORT || 1521);
+console.log('Username:', process.env.DB_USER || 'rm88430');
+console.log('Password:', process.env.DB_PASSWORD || '040302');
+console.log('Database:', process.env.DB_NAME || 'orcl');
+
 // Configurar a conexão com o banco de dados Oracle
-const sequelize = new Sequelize({
-  dialect: 'oracle',
-  host: process.env.DB_HOST || 'oracle.fiap.com.br',
-  port: process.env.DB_PORT || 1521,
-  database: process.env.DB_NAME || 'orcl',
-  username: process.env.DB_USER || 'rm88430',
-  password: process.env.DB_PASSWORD || '040302',
-});
+const sequelize = new Sequelize(
+  `oracle://${process.env.DB_USER || 'rm88430'}:${process.env.DB_PASSWORD || '040302'}@${process.env.DB_HOST || 'oracle.fiap.com.br'}:${process.env.DB_PORT || 1521}/${process.env.DB_NAME || 'orcl'}`
+);
 
 async function connect() {
   try {
